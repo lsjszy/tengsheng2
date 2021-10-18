4d48accbbfa6b0a2608b1cea79394849928d7fff
zwt1128 <298206789@qq.com>
2021-10-18 14:12:51 +0800

    测试

--- public/js/go_top.js
+++ public/js/go_top.js
@@ -1,27 +1,28 @@
-$('.swiper-wrapper').bind('mouseenter', function() {
+$('.swiper-wrapper').bind('mouseenter', function () {
     $('.swiper-button-next').fadeIn();
     $('.swiper-button-prev').fadeIn();
 })
-$('.swiper-wrapper').bind('mouseleave', function() {
+$('.swiper-wrapper').bind('mouseleave', function () {
     $('.swiper-button-next').fadeOut();
     $('.swiper-button-prev').fadeOut();
 })
-$('#close_im').bind('click', function() {
+$('#close_im').bind('click', function () {
     $('#main-im').css("height", "0");
     $('#im_main').hide();
     $('#open_im').show();
 });
-$('#open_im').bind('click', function(e) {
+
+$('#open_im').bind('click', function (e) {
     $('#main-im').css("height", "272");
     $('#im_main').show();
     $(this).hide();
 });
 // 返回顶部
-$('.go-top').bind('click', function() {
+$('.go-top').bind('click', function () {
     $(window).scrollTop(0);
 });
 // 返回顶部
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(document).scrollTop() >= 300) {
         $('.go-top').fadeIn();
     } else {
